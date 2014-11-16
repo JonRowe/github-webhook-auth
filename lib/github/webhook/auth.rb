dd33f83b6bb90be653634d8057f57370b82060cd
Jon Rowe <hello@jonrowe.co.uk>
2014-11-16 16:56:34 +0800

    deny access to people who dont have the correct token

--- lib/github/webhook/auth.rb
+++ lib/github/webhook/auth.rb
@@ -1,3 +1,5 @@
+require 'rack/utils'
+
 module Github
   module Webhook
     class Auth
@@ -6,8 +8,29 @@ module Github
       end
 
       def call env
+        return [401, {}, ''] unless verify_signature(env.dup)
         @app.call env
       end
+
+    private
+
+      def verify_signature env
+        Rack::Utils.secure_compare signature(body env), env.fetch('HTTP_X_HUB_SIGNATURE','')
+      end
+
+      def body env
+        env['rack.input'].rewind
+        env['rack.input'].read
+      end
+
+      def signature body
+        'sha1=' + OpenSSL::HMAC.hexdigest(digest, ENV['SECRET_TOKEN'], body)
+      end
+
+      def digest
+        OpenSSL::Digest.new('sha1')
+      end
+
     end
   end
 end
--- spec/github/webhook/auth_spec.rb
+++ spec/github/webhook/auth_spec.rb
@@ -3,13 +3,50 @@ require 'github-webhook-auth'
 describe 'protecting a webhook api endpoint' do
 
   let(:app)        { double 'rack stack', :call => result }
-  let(:env)        { double 'env' }
   let(:result)     { double 'result' }
   let(:middleware) { Github::Webhook::Auth.new(app) }
+  let(:secret)     { 'S0m3Key' }
 
-  it 'calls the app and returns its response' do
-    expect(app).to receive(:call).with(env)
-    expect(middleware.call(env)).to eq result
+  before do
+    stub_const("ENV", 'SECRET_TOKEN' => secret)
+  end
+
+  context 'when a valid auth token is provided' do
+    let(:env)       { { 'HTTP_X_HUB_SIGNATURE' => signature, 'rack.input' => body } }
+    let(:body)      { StringIO.new }
+    let(:hexdigest) { OpenSSL::Digest::Digest.new('sha1') }
+    let(:signature) { 'sha1='+OpenSSL::HMAC.hexdigest(hexdigest, secret, body.read) }
+
+    it 'calls the app and returns its response' do
+      expect(app).to receive(:call).with(env)
+      expect(middleware.call(env)).to eq result
+    end
+  end
+
+  context 'when an invalid auth token is provided' do
+    let(:env) { { 'HTTP_X_HUB_SIGNATURE' => 'notatoken', 'rack.input' => StringIO.new } }
+
+    it 'short circuits the app' do
+      expect(app).to_not receive(:call).with(env)
+      middleware.call(env)
+    end
+
+    it 'returns a 401 status' do
+      expect(middleware.call(env)[0]).to eq 401
+    end
+  end
+
+  context 'when no auth token is provided' do
+    let(:env) { { 'rack.input' => StringIO.new } }
+
+    it 'short circuits the app' do
+      expect(app).to_not receive(:call).with(env)
+      middleware.call(env)
+    end
+
+    it 'returns a 401 status' do
+      expect(middleware.call(env)[0]).to eq 401
+    end
   end
 
 end
